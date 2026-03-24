ec2b210f7be5fa3378bed0505135c37bae18808e
mayankatwork224 <mayankatwork224@gmail.com>
2026-03-24 14:05:34 +0000

    add docs

--- src/app/bootstrap/express/expressServer.ts
+++ src/app/bootstrap/express/expressServer.ts
@@ -1,28 +1,26 @@
 import express from "express";
-import {Express} from "express";
+import { Express } from "express";
 import cors from "cors";
 import { handleExpressError } from "../exceptions/handleExpressError";
-import { NextFunction, Response, Request} from "express";
+import { NextFunction, Response, Request } from "express";
 
+export function expressServer(app: Express, PORT: number) {
+  app.use(
+    cors({
+      origin: "*",
+      credentials: true,
+    }),
+  );
 
-export function expressServer(app:Express, PORT:number){
-    
-    app.use(cors({
-        origin: "*",
-        credentials : true
-    }))
+  app.use(express.json());
+  app.use(express.urlencoded({ extended: true }));
 
+  app.get("/", (req: Request, res: Response) => {
+    res.json({ "message": "express server is up and running" });
+  });
+  app.use(handleExpressError);
 
-    app.use(express.json())
-    app.use(express.urlencoded({extended:true}))
-
-    app.use(handleExpressError)
-
-    app.get("/", (req: Request,res:Response) => {
-        res.json({message: "express server is up and running"})
-    })
-
-    app.listen(PORT, () => {
-        console.log(`Express server is running at http://127.0.0.1:8000`)
-    })
+  app.listen(PORT, () => {
+    console.log(`Express server is running at http://127.0.0.1:8000`);
+  });
 }
